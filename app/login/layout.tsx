bc54025fcb771039ab73e6b9593eafe0551b70ad
Jeongjun <jeongjun.business@gmail.com>
2025-01-16 01:06:01 +0900

    [FEATURE] 로그인 상태 관리를 위한 커스텀 Hook과 Ccontext 구현
    - 로컬스토리지 저장 데이터(액세스 토큰, user 정보)를 통해 로그인 여부 체크
    - 이미 로그인 된 사용자 대시보드로 리다이렉트 처리 (관리자, 회원 별도
      페이지)

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -1,29 +1,34 @@
-import type { Metadata } from "next";
+"use client"; // 클라이언트 상태 관리를 위해 필요
+
+import { RedirectProvider } from "@/src/context/RedirectContext";
+// import type { Metadata } from "next";
 import { Provider } from "@/src/components/ui/provider";
 import { Box, Flex } from "@chakra-ui/react";
 
-export const metadata: Metadata = {
-  title: "FlowSync",
-  description: "The World Best PMS Service",
-};
+// export const metadata: Metadata = {
+//   title: "FlowSync",
+//   description: "The World Best PMS Service",
+// };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <Provider>
-      {/* 로그인 페이지 중앙 정렬 레이아웃 */}
-      <Flex
-        direction="column"
-        align="center"
-        justify="center"
-        minHeight="100vh"
-        backgroundColor="gray.50"
-        padding="3"
-        overflow="hidden" // 스크롤 제거
-      >
-        <Box width="100%" maxW="500px" borderRadius="md" bg="white" boxShadow="lg" padding="1">
-          {children}
-        </Box>
-      </Flex>
+      <RedirectProvider>
+        {/* 로그인 페이지 중앙 정렬 레이아웃 */}
+        <Flex
+          direction="column"
+          align="center"
+          justify="center"
+          minHeight="100vh"
+          backgroundColor="gray.50"
+          padding="3"
+          overflow="hidden" // 스크롤 제거
+        >
+          <Box width="100%" maxW="500px" borderRadius="md" bg="white" boxShadow="lg" padding="1">
+            {children}
+          </Box>
+        </Flex>
+      </RedirectProvider>
     </Provider>
   );
 };
---
+++ src/hook/useRedirectIfLoggedIn.ts
@@ -0,0 +1,24 @@
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useRedirectContext } from "@/src/context/RedirectContext";
+
+export function useRedirectIfLoggedIn() {
+  const { redirectPath } = useRedirectContext(); // Context에서 리다이렉트 경로 가져오기
+  const router = useRouter(); // Next.js의 useRouter 훅 사용
+
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+    const userString = localStorage.getItem("user");
+    const user = userString ? JSON.parse(userString) : null;
+
+    if (token && user) {
+      // 사용자가 관리자라면 관리자 대시보드 페이지로 리다이렉트
+      if (user.role === "ROLE_ADMIN") {
+        router.push("/admin");
+      } else {
+        // 사용자가 회원일 경우 회원 대시보드 페이지로 리다이렉트
+        router.push(redirectPath);
+      }
+    }
+  }, [redirectPath, router]);
+}
