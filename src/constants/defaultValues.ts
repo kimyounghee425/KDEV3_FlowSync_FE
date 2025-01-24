0a3c8a51813bd07ff2822125799e508234c81357
Jeongjun <jeongjun.business@gmail.com>
2025-01-24 23:53:53 +0900

    REFACTOR - 공통 컴포넌트 내 스타일링 코드 별도 CSS 파일로 분리, handleSubmit 함수 기능ㅇ ㅔ 코드 컨벤션 정리,

---
+++ src/constants/defaultValues.ts
@@ -0,0 +1,15 @@
+// 업체 생성 페이지 - 입력 폼 초기값 설정
+export const defaultValuesOfOrganizaion = {
+  name: "",
+  brNumber: "",
+  brCertificateUrl: "https://",
+  streetAddress: "",
+  detailAddress: "",
+  phoneNumber: "",
+  type: "CUSTOMER",
+};
+
+export const defaultValuesOfLogin = {
+  email: "",
+  password: "",
+};
---
+++ src/constants/validationRules.ts
@@ -0,0 +1,45 @@
+// 업체 생성 페이지 - 유효성 검증 규칙
+export const validationRulesOfCreatingOrganization = {
+  name: {
+    isValid: (value: string) => value.trim() !== "", // trim(): value에서 공백이 모두 제거된 값을 반환
+    errorMessage: "업체명을 입력하세요.",
+  },
+  brNumber: {
+    isValid: (value: string) => /^\d{3}-\d{2}-\d{5}$/.test(value),
+    errorMessage: "올바른 사업자 등록번호를 입력하세요.",
+  },
+  brCertificateUrl: {
+    isValid: (value: string) => /^https?:\/\/.+\..+$/.test(value),
+    errorMessage: "올바른 회사 URL을 입력하세요.",
+  },
+  streetAddress: {
+    isValid: (value: string) => value.trim() !== "",
+    errorMessage: "사업장 도로명 주소를 입력하세요.",
+  },
+  detailAddress: {
+    isValid: (value: string) => value.trim() !== "",
+    errorMessage: "사업장 상세 주소를 입력하세요.",
+  },
+  phoneNumber: {
+    isValid: (value: string) => /^\d{3}-\d{4}-\d{4}$/.test(value),
+    errorMessage: "올바른 전화번호를 입력하세요.",
+  },
+  type: {
+    isValid: (value: string) => ["CUSTOMER", "DEVELOPER"].includes(value),
+    errorMessage: "업체 유형을 선택하세요.",
+  },
+};
+
+// 로그인 페이지 - 유효성 검증 규칙
+export const validationRulesOfLogin = {
+  email: {
+    isValid: (value: string) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value),
+    errorMessage: "올바른 이메일 주소를 입력하세요.",
+  },
+  // password: {
+  //   isValid: (value: string) =>
+  //     /^(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()\-_=+{};:,<.>]).{8,}$/.test(value),
+  //   errorMessage:
+  //     "영문, 숫자, 특수문자를 포함하여 최소 8자리 이상 비밀번호를 입력하시기 바랍니다.",
+  // },
+};
