29ea58b785a4a64262be06d8a9e5cd586b50a32a
doctorsean <55120852+doctorsean@users.noreply.github.com>
2025-01-27 20:27:11 +0900

    뒤로가기 버튼 활성화

--- src/components/common/backButton.tsx
+++ src/components/common/backButton.tsx
@@ -1,17 +1,20 @@
 import { Box, Button } from "@chakra-ui/react";
 import { BiArrowBack } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
 
-const BackButton = () => (
-  <Box position={"relative"} w={"100"} mb={4}>
-    <Button
-      bg={"white"}
-      color={"black"}
-      colorScheme={"blackAlpha"}
-      _hover={{ bg: "blackAlpha.300" }}
-    >
-      <BiArrowBack />
-    </Button>
-  </Box>
-);
+export default function BackButton() {
 
-export default BackButton;
+  return (
+    <Box position={"relative"} w={"100"} mb={4}>
+      <Button
+        bg={"white"}
+        color={"black"}
+        colorScheme={"blackAlpha"}
+        _hover={{ bg: "blackAlpha.300" }}
+        onClick={() => window.history.back()}
+      >
+        <BiArrowBack />
+      </Button>
+    </Box>
+  );
+}
