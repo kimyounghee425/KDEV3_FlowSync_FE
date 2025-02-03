dca6e449d7f7ee5d58bd9f11da4761cb4ee31167
Jeongjun <jeongjun.business@gmail.com>
2025-02-03 22:54:46 +0900

    로그인 페이지 반응형 디자인 구현 및 로그인 API 연동

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,8 +1,14 @@
 import axiosInstance from "@/src/api/axiosInstance";
-import { CommonResponseType, ReissueResponse, UserInfoResponse } from "@/src/types";
+import {
+  CommonResponseType,
+  ReissueResponse,
+  UserInfoResponse,
+} from "@/src/types";
 
 // 사용자 권한 API 호출
-export async function fetchUserInfo(accessToken?: string): Promise<CommonResponseType<UserInfoResponse>> {
+export async function fetchUserInfo(
+  accessToken?: string,
+): Promise<CommonResponseType<UserInfoResponse>> {
   const response = await axiosInstance.get("/me", {
     headers: {
       "Content-Type": "application/json",
@@ -11,12 +17,14 @@ export async function fetchUserInfo(accessToken?: string): Promise<CommonRespons
     withCredentials: true, // ✅ 클라이언트 환경에서도 쿠키 포함
     validateStatus: (status) => status < 500, // 🔹 500 이상만 오류로 처리
   });
-  
+
   return response.data;
 }
 
 // 🔹 Refresh Token을 포함하여 토큰 재발급 요청
-export async function fetchReissueToken(refreshToken?: string): Promise<CommonResponseType<ReissueResponse>> {
+export async function fetchReissueToken(
+  refreshToken?: string,
+): Promise<CommonResponseType<ReissueResponse>> {
   const response = await axiosInstance.get("/reissue", {
     headers: {
       "Content-Type": "application/json",
@@ -31,9 +39,19 @@ export async function fetchReissueToken(refreshToken?: string): Promise<CommonRe
 
 // 로그인 API 호출 => 액세스 토큰 & user 정보 반환
 export async function login(email: string, password: string) {
-  const response = await axiosInstance.post("/login", { email, password });
-  // console.log("로그인 API 응답 완료 - response: ", response);
-  return response;
+  try {
+    const response = await axiosInstance.post("/login", { email, password });
+    if (response.data.code === 200 && response.data.result === "SUCCESS") {
+      return response.data; // 성공 응답 반환
+    } else {
+      // 실패 메시지 처리
+      throw new Error(response.data.message || "로그인에 실패하였습니다.");
+    }
+  } catch (error: any) {
+    console.error("API 호출 에러:", error.message || error);
+    alert("로그인에 실패했습니다. 이메일 또는 비밀번호를 다시 확인하세요.");
+    throw error;
+  }
 }
 
 // 로그아웃 API 호출
