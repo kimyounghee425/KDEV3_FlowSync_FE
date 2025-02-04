b421e86f9cdb40f958a4e7f7f992c5feece65955
MUNJEONGJUN <102046445+MUNJEONGJUN@users.noreply.github.com>
2025-02-04 09:51:48 +0900

    로그아웃 API 연동 (#122)
    * 로그인 페이지 반응형 디자인 구현 및 로그인 API 연동
    
    * 헤더 레이아웃 변경 및 로그인 사용자 프로필 조회 API 연동
    
    * 로그아웃 API 연동 완료
    
    * 불필요한 쿠키/로컬스토리지 삭제 코드 및  외부 리소스 캐싱 방지 코드 추가

--- src/components/layouts/Profile.tsx
+++ src/components/layouts/Profile.tsx
@@ -37,7 +37,10 @@ export default function Profile() {
             role: response.data.data.role,
           });
           // 랜덤 프로필 이미지 설정
-          setProfileImageUrl(getRandomProfileImage());
+          // 프로필 이미지를 외부 URL에서 가져올 때, URL 뒤에 캐싱 방지 쿼리 문자열(timestamp) 를 추가하여 브라우저 캐시 무력화
+          setProfileImageUrl(
+            `${getRandomProfileImage()}?timestamp=${Date.now()}`,
+          );
         }
       } catch (error: any) {
         console.error("사용자 데이터를 가져오지 못했습니다.", error);
@@ -50,6 +53,41 @@ export default function Profile() {
   if (!userData) {
     return <Loading />;
   }
+
+  const handleLogout = async () => {
+    try {
+      const response = await axiosInstance.post("/logout");
+      if (response.data.code === 200 && response.data.result === "SUCCESS") {
+        // 브라우저의 쿠키 삭제
+        // document.cookie =
+        //   "authToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        // document.cookie =
+        //   "refreshToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        const clearCookies = () => {
+          const cookies = document.cookie.split("; ");
+          cookies.forEach((cookie) => {
+            const [key] = cookie.split("=");
+            document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=${window.location.hostname};`;
+          });
+        };
+        clearCookies();
+
+        // 로컬 스토리지 초기화
+        localStorage.removeItem("user");
+        localStorage.removeItem("projectStatus");
+
+        alert("로그아웃 되었습니다.");
+        // 강제로 새로고침하면서 로그인 페이지로 이동
+        // route.push() 의 경우, 브라우저 캐싱 때문에 로그인 페이지로 이동해도 기존 쿠키 값이 보여짐
+        window.location.href = "/login";
+      } else {
+        throw new Error(response.data.message || "로그아웃에 실패하였습니다.");
+      }
+    } catch (error: any) {
+      console.error("로그아웃 실패:", error);
+      alert("로그아웃에 실패하였습니다. 다시 시도해주세요.");
+    }
+  };
 
   return (
     <Box
@@ -84,6 +122,7 @@ export default function Profile() {
               value="로그아웃"
               color="fg.error"
               _hover={{ bg: "bg.error", color: "fg.error" }}
+              onClick={handleLogout}
             >
               <Link href={"/"}>로그아웃</Link>
             </MenuItem>
