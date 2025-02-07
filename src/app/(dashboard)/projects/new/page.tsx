bb11e278a30eac9ac3c8bcf83df019ba0bfdc2d3
MUNJEONGJUN <102046445+MUNJEONGJUN@users.noreply.github.com>
2025-02-07 22:02:58 +0900

    Update page.tsx

--- src/app/(dashboard)/projects/new/page.tsx
+++ src/app/(dashboard)/projects/new/page.tsx
@@ -1,6 +1,6 @@
 // 프로젝트 생성 페이지
 
-import ProjectRegisterPage from "@/src/pages/ProjectRegisterPage";
+import ProjectRegisterPage from "@/src/components/pages/ProjectRegisterPage";
 
 export default function page() {
   return <ProjectRegisterPage />;
